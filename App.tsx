aac94e7e90f34df8349f126ca82d679fb554605b
buffalokusojima <australian.in.tus@icloud.com>
2021-07-30 20:20:46 +0900

    [fix] header and gitignore

--- App.tsx
+++ App.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { SafeAreaView, StyleSheet, Text, View } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
-import { createStackNavigator } from "@react-navigation/stack";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 
 import AuthStateApp from "./utils/AuthStateApp";
