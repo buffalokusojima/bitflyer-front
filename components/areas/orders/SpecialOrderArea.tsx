582748779803e3b2cb983508ccc47a0f753e053f
buffalokusojima <australian.in.tus@icloud.com>
2021-08-01 21:20:23 +0900

    [fix] DropDown modified

--- components/areas/orders/SpecialOrderArea.tsx
+++ components/areas/orders/SpecialOrderArea.tsx
@@ -1,6 +1,9 @@
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, Platform } from "react-native";
 import { Picker } from "@react-native-picker/picker";
+// import { Picker } from "@react-native-community/picker";
+
+import SelectDropdown from "react-native-select-dropdown";
 
 import BaseOrderArea from "./BaseOrderArea";
 import {
@@ -22,7 +25,9 @@ const styles = StyleSheet.create({
 
 const SpecialOrderArea: React.FC = () => {
   const [alertMessage, setAlertMessage] = React.useState("");
-  const [selectedOrder, setSelectedOrder] = React.useState("");
+  const [selectedOrder, setSelectedOrder] = React.useState();
+
+  const ordertypes = ["IFDBUY", "IFDSELL", "STOP", "IFDOCOBUY", "IFDOCOSELL"];
 
   return (
     <>
@@ -30,18 +35,50 @@ const SpecialOrderArea: React.FC = () => {
       <BaseOrderArea>
         <View style={{ flexDirection: "row" }}>
           <Text style={styles.heading}>Special Order</Text>
+          {Platform.OS === "ios" ? (
+            <SelectDropdown
+              data={ordertypes}
+              onSelect={(selectedItem, index) => {
+                setSelectedOrder(selectedItem);
+              }}
+              buttonTextAfterSelection={(selectedItem, index) => {
+                // text represented after item is selected
+                // if data array is an array of objects then return selectedItem.property to render after item is selected
+                return selectedItem;
+              }}
+              rowTextForSelection={(item, index) => {
+                // text represented for each item in dropdown
+                // if data array is an array of objects then return item.property to represent item in dropdown
+                return item;
+              }}
+            />
+          ) : (
+            <Picker
+              selectedValue={selectedOrder}
+              onValueChange={(itemValue, itemIndex) =>
+                setSelectedOrder(itemValue)
+              }
+            >
+              <Picker.Item label="IFDBUY" value="IFDBUY" />
+              <Picker.Item label="IFDSELL" value="IFDSELL" />
+              <Picker.Item label="STOP" value="STOP" />
+              <Picker.Item label="IFDOCOBUY" value="IFDOCOBUY" />
+              <Picker.Item label="IFDOCOSELL" value="IFDOCOSELL" />
+            </Picker>
+          )}
+
+          {/* <View style={styles.picker}>
           <Picker
             selectedValue={selectedOrder}
+            style={{ height: 50, width: 100 }}
             onValueChange={(itemValue, itemIndex) =>
               setSelectedOrder(itemValue)
             }
           >
-            <Picker.Item label="IFDBUY" value="IFDBUY" />
-            <Picker.Item label="IFDSELL" value="IFDSELL" />
-            <Picker.Item label="STOP" value="STOP" />
-            <Picker.Item label="IFDOCOBUY" value="IFDOCOBUY" />
-            <Picker.Item label="IFDOCOSELL" value="IFDOCOSELL" />
+            <Picker.Item label="Java" value="java" />
+            <Picker.Item label="JavaScript" value="js" />
           </Picker>
+          {/* </View> */}
         </View>
         {selectedOrder === "IFDSELL" ? (
           <IFDSellOrderForm setAlert={setAlertMessage} />
